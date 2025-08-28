16e6a191285f00f8d99b7ca33b0e095ee86723af
Kayky Kenji <88341735+kakenji@users.noreply.github.com>
2025-08-28 18:42:15 -0300

    Add files via upload

--- mongodb.js
+++ mongodb.js
@@ -6,7 +6,7 @@ const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri, {
   serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true },
   tls: true,
-  tlsAllowInvalidCertificates: false
+  tlsAllowInvalidCertificates: true
 });
 
 let db;
