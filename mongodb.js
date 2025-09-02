dc492ff11c14671870f0e8718e5f5f7fd8b60c29
Kayky Kenji <88341735+kakenji@users.noreply.github.com>
2025-09-01 21:50:25 -0300

    Add files via upload

--- mongodb.js
+++ mongodb.js
@@ -5,8 +5,7 @@ dotenv.config();
 const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri, {
   serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true },
-  tls: true,
-  tlsAllowInvalidCertificates: true
+  tls: true
 });
 
 let db;
